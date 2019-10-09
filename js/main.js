355eae643f565d8d433398ef89e3db713d25fbfb
Siarhei-Chaptsau <s.chaptsau@gmail.com>
2019-10-10 02:07:01 +0300

    Личный проект: подробности

--- js/main.js
+++ js/main.js
@@ -135,8 +135,10 @@ var pinLevelEffect = document.querySelector('.effect-level__pin');
 var pinLevelValue = document.querySelector('.effect-level__value');
 var effectsItem = document.querySelectorAll('.effects__item');
 
-// var scaleSmallerButton = document.querySelector('.scale__control--smaller');
-// var scaleBiggerButton = document.querySelector('.scale__control--bigger');
+var scaleButtons = document.querySelector('.img-upload__scale');
+var imgUploadPreview = document.querySelector('.img-upload__preview');
+var scaleValueButton = document.querySelector('.scale__control--value');
+
 // var effectsList = document.querySelector('.effects__list');
 // var noneEffect = effectsList.querySelector('#effect-none');
 // var chromeEffect = effectsList.querySelector('#effect-chrome');
@@ -144,8 +146,43 @@ var effectsItem = document.querySelectorAll('.effects__item');
 // var marvinEffect = effectsList.querySelector('#effect-marvin');
 // var phobosEffect = effectsList.querySelector('#effect-phobos');
 // var heatEffect = effectsList.querySelector('#effect-heat');
-//
+
 // var inputHashtags = document.querySelector('.text__hashtags');
+
+var lib = {
+  'effects__preview--chrome': {
+    name: 'grayscale',
+    setPercent: function (val) {
+      return val / 100;
+    }
+  },
+  'effects__preview--sepia': {
+    name: 'sepia',
+    setPercent: function (val) {
+      return val / 100;
+    }
+  },
+  'effects__preview--marvin': {
+    name: 'invert',
+    setPercent: function (val) {
+      return val + '%';
+    }
+  },
+  'effects__preview--phobos': {
+    name: 'blur',
+    setPercent: function (val) {
+      return val * 0.03 + 'px';
+    },
+    min: 0,
+    max: '3px'
+  },
+  'effects__preview--heat': {
+    name: 'brightness',
+    setPercent: function (val) {
+      return val * 0.02 + 1;
+    }
+  }
+};
 
 var onPopupEscPress = function (evt) {
   if (evt.keyCode === ESC_KEYCODE) {
@@ -178,17 +215,58 @@ btnUploadCancel.addEventListener('keydown', function (evt) {
   }
 });
 
-window.pinLevel = pinLevelValue.value;
-// Math.round((evt.clientX - START_X_PIN_LEVEL) / (END_X_PIN_LEVEL - START_X_PIN_LEVEL) * 100));
+scaleValueButton.value = '100%'; // ?
 
+scaleButtons.addEventListener('click', function (evt) {
+  var percent = parseInt(scaleValueButton.value, 10);
+
+  if (evt.target.matches('.scale__control--bigger')) {
+    percent += 25;
+    if (percent >= 100) {
+      percent = 100;
+    }
+  } else if (evt.target.matches('.scale__control--smaller')) {
+    percent -= 25;
+    if (percent < 25) {
+      percent = 25;
+    }
+  }
+
+  imgUploadPreview.style = 'transform:scale(' + percent / 100 + ')';
+  scaleValueButton.value = percent + '%';
+});
+
+
+// window.pinLevel = pinLevelValue.value;
+// Math.round((evt.clientX - START_X_PIN_LEVEL) / (END_X_PIN_LEVEL - START_X_PIN_LEVEL) * 100));
 pinLevelEffect.addEventListener('mouseup', function () {
   // window.pinLevel = mouseup.value;
 });
 
-var effectsItemClickHandler = function () {
-  // reset pin
+// изменение превью фото при нажатии на эффект
+var setPhotoEffectHandler = function (evt) {
+  if (evt.target.matches('.effects__preview')) {
+    var arrClassForPreviewPhoto = evt.target.className.split('  ');
+
+    // уровень эффекта cбрасывается до начального состояния
+    pinLevelValue.setAttribute('value', '100');
+
+    // скидываем предыдущие классы превью фото:
+    imgUploadPreview.classList = 'img-upload__preview';
+
+    // добаввляем превью фото класс эффекта из массива классов элемента на которое произошло нажатие:
+    arrClassForPreviewPhoto.forEach(function (item) {
+      if (item !== 'effects__preview') {
+        imgUploadPreview.classList.add(item);
+
+        if (item !== 'effects__preview--none') {
+          imgUploadPreview.style = 'filter:' + lib[item.value].name + '(' + lib[item.value].setPercent(pinLevelValue.value) + ')';
+        }
+      }
+    });
+  }
 };
 
 for (var y = 0; y < effectsItem.length; y++) {
-  effectsItem[y].addEventListener('click', effectsItemClickHandler);
+  effectsItem[y].addEventListener('click', setPhotoEffectHandler);
 }
